21d4d59e95d66945d97ad81681e1c1dc43fc46c6
gmawji <git@gmawji.com>
2017-06-14 05:00:56 -0400

    foreach control structure.

--- js/app.js
+++ js/app.js
@@ -3,6 +3,12 @@ var ViewModel = function () {
     this.name = ko.observable('Tabby');
     this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
     this.imgAttribution = ko.observable('Udacity')
+    this.nicknames = ko.observableArray([
+        { name: 'Tabtab'},
+        { name: 'T-Bone'},
+        { name: 'Mr. T'},
+        { name: 'Tabitha Tab Tabby Catty Cat'}
+        ]);
 
     this.title = ko.computed(function() {
         var self = this;
