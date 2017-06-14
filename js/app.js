6881f1bc4e5f7a930a2411cc526b70b260682e39
gmawji <git@gmawji.com>
2017-06-14 05:45:07 -0400

    Prep for adding more cats.

--- js/app.js
+++ js/app.js
@@ -1,14 +1,9 @@
-var Cat = function() {
-    this.clickCount = ko.observable(0);
-    this.name = ko.observable('Tabby');
-    this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-    this.imgAttribution = ko.observable('Udacity')
-    this.nicknames = ko.observableArray([
-        { name: 'Tabtab'},
-        { name: 'T-Bone'},
-        { name: 'Mr. T'},
-        { name: 'Tabitha Tab Tabby Catty Cat'}
-        ]);
+var Cat = function(data) {
+    this.clickCount = ko.observable(data.clickCount);
+    this.name = ko.observable(data.name);
+    this.imgSrc = ko.observable(data.imgSrc);
+    this.imgAttribution = ko.observable(data.imgAttribution);
+    this.nicknames = ko.observableArray(data.nicknames);
 
     this.title = ko.computed(function() {
         var self = this;
@@ -33,7 +28,13 @@ var Cat = function() {
 
 var ViewModel = function () {
 
-    this.currentCat = ko.observable( new Cat() );
+    this.currentCat = ko.observable( new Cat({
+        clickCount: 0,
+        name: 'Tabby',
+        imgSrc: 'img/434164568_fea0ad4013_z.jpg',
+        imgAttribution: 'Udacity',
+        nicknames: ['Tabtab', 'T-Bone', 'Mr. T', 'Tabitha Tab Tabby Catty Cat']
+    }) );
 
     this.incrementCounter = function() {
         this.clickCount(this.clickCount() +1);
