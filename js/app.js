b42344e781a9091f6795c67fb73c94c2a4406651
gmawji <git@gmawji.com>
2017-06-14 06:07:31 -0400

    Added more cats!

--- js/app.js
+++ js/app.js
@@ -1,3 +1,41 @@
+var initialCats = [
+    {
+        clickCount: 0,
+        name : 'Slim',
+        imgSrc : 'img/cat1.jpg',
+        imgAttribution: 'Udacity',
+        nicknames: ['Skinny']
+    },
+    {
+        clickCount: 0,
+        name : 'Shady',
+        imgSrc : 'img/cat2.jpg',
+        imgAttribution: 'Udacity',
+        nicknames: ['Shadow']
+    },
+    {
+        clickCount: 0,
+        name : 'Peabody',
+        imgSrc : 'img/cat3.jpg',
+        imgAttribution: 'Udacity',
+        nicknames: ['Posh']
+    },
+    {
+        clickCount: 0,
+        name : 'Zlatan',
+        imgSrc : 'img/cat4.jpg',
+        imgAttribution: 'Udacity',
+        nicknames: ['King']
+    },
+    {
+        clickCount: 0,
+        name : 'Krish',
+        imgSrc : 'img/cat5.jpg',
+        imgAttribution: 'Chelsea',
+        nicknames: ['Kush']
+    }
+];
+
 var Cat = function(data) {
     this.clickCount = ko.observable(data.clickCount);
     this.name = ko.observable(data.name);
@@ -27,18 +65,23 @@ var Cat = function(data) {
 }
 
 var ViewModel = function () {
+    var self = this;
 
-    this.currentCat = ko.observable( new Cat({
-        clickCount: 0,
-        name: 'Tabby',
-        imgSrc: 'img/434164568_fea0ad4013_z.jpg',
-        imgAttribution: 'Udacity',
-        nicknames: ['Tabtab', 'T-Bone', 'Mr. T', 'Tabitha Tab Tabby Catty Cat']
-    }) );
+    this.catList = ko.observableArray([]);
+
+    initialCats.forEach(function(catItem) {
+        self.catList.push( new Cat(catItem) );
+    });
+
+    this.currentCat = ko.observable( this.catList()[0] );
 
     this.incrementCounter = function() {
         this.clickCount(this.clickCount() +1);
     };
+
+    this.setCat = function(clickedCat) {
+        self.currentCat(clickedCat)
+    }
 };
 
 ko.applyBindings(new ViewModel());
